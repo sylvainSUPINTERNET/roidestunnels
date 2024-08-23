eb553b8fba82f08b214db5dc699264ed2c09f210
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-08-23 20:00:21 +0200

    GO

--- app/components/mealbuilder.component.tsx
+++ app/components/mealbuilder.component.tsx
@@ -5,6 +5,7 @@ import { FiAlertOctagon, FiArrowLeft } from "react-icons/fi"
 
 export default function MealBuilder({offerTypeSelected, setCurrentPage, pack} : {offerTypeSelected: string, setCurrentPage: React.Dispatch<React.SetStateAction<string>>, pack:any}) {
     
+    const max_topping_per_meal:number = 4;
 
      let [ selected, setSelected] = useState<Set<string>>(new Set()); 
 
@@ -97,6 +98,12 @@ export default function MealBuilder({offerTypeSelected, setCurrentPage, pack} : 
     }
 
 
+    const detectAllFilled = () => {
+        // TODO => rajouter dans les meta data number de repas
+        // virer le truck hardcodé ligne 126 
+        // l'idée c'est que on fait max_topping * number de repas si egal au size de selected c'est que tout est bien remplis on peut passer au paiement
+    }
+
 
 
     useEffect( () => {
@@ -107,6 +114,7 @@ export default function MealBuilder({offerTypeSelected, setCurrentPage, pack} : 
     return ( 
         <div>
             
+            {JSON.stringify(pack)}
             <div onClick={ e => {setCurrentPage("main")}} className="cursor-pointer flex items-center p-2">
                 <FiArrowLeft fontSize={36} className=" bg-white p-1 "/>
                 <p className="text-2xl bg-white p-1 text-zinc-600/80" >
