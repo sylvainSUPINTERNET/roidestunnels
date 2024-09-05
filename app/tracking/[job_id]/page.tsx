576ae4304bf2767806463c1c5cb7197f5e185125
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2024-09-05 21:59:26 +0200

    WIP

--- app/tracking/[job_id]/page.tsx
+++ app/tracking/[job_id]/page.tsx
@@ -1,3 +1,8 @@
-export default function Page({ params }: { params: { job_id: string } }) {
-    return <div>My Post: {params.job_id}</div>
+export default async function Page({ params }: { params: { job_id: string } }) {
+
+    
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    let posts = await data.json()
+
+    return <div>My Post: {params.job_id} = {JSON.stringify(posts)}</div>
 }
