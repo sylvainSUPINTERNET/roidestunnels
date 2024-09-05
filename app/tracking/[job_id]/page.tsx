17988a7db91ed166b6492223f170b466bed81d3d
sylvain <sylvain@macbook-pro-de-sylvain.home>
2024-09-05 23:21:16 +0200

    wip

--- app/tracking/[job_id]/page.tsx
+++ app/tracking/[job_id]/page.tsx
@@ -1,8 +1,19 @@
 export default async function Page({ params }: { params: { job_id: string } }) {
 
     
-    const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    let posts = await data.json()
+    // const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    // let posts = await data.json()
 
-    return <div>My Post: {params.job_id} = {JSON.stringify(posts)}</div>
+    return <div>
+            <div className="flex justify-center items-center h-screen">
+                <div className="bg-gray-200 rounded w-11/12 h-5/6 flex justify-center items-center">
+                    <iframe 
+                    src="https://stuart.sandbox.followmy.delivery/100795284/cae994e5646060db64e8be466f877876"
+                    className="w-full h-full rounded p-2 shadow-lg"
+                    title="Suivi de livraison"
+                    allowFullScreen
+                    ></iframe>
+                </div>
+            </div>
+    </div>
 }
